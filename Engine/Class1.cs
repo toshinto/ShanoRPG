a45864a2bc7970bb9138174ad522aaa9826e0d06
ixtreon <ixtreon@gmail.com>
2014-08-25 13:21:16 +0300

    Added blank objects

--- Engine/Class1.cs
+++ Engine/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.GameObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,6 @@ namespace Engine
 {
     public class Class1
     {
+        Entity e;
     }
 }
