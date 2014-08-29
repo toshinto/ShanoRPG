7f647658d87a45cb99e5b4730a9b3d840bd6aea0
toshinto <lyvcheto@abv.bg>
2014-08-29 13:05:35 +0300

    Added buff handlers

--- Engine/GameObjects/Entity.cs
+++ Engine/GameObjects/Entity.cs
@@ -11,13 +11,21 @@ namespace Engine.GameObjects
         public readonly string Name;
         public abstract int Level { get; }
 
+        public double CurrentAttackCooldown
+        {
+            get { return 1 / CurrentAttacksPerSecond;  }
+        }
+        
 
+        public double CurrentAttacksPerSecond { get; protected set; }
+        public double CurrentMinDamage { get; protected set; }
         public double CurrentDefense { get; protected set; }
         public double CurrentDodge { get; protected set; }
         public double MaxLife { get; protected set; }
         public double MaxMana { get; protected set; }
-        public double CurrentDamage { get; protected set; }
+        public double CurrentMaxDamage { get; protected set; }
         public double CurrentMoveSpeed { get; protected set; }
+
 
 
         public List<Buff> Buffs = new List<Buff>();
@@ -31,15 +39,43 @@ namespace Engine.GameObjects
             BaseDefense,
             BaseDodge,
             BaseMoveSpeed,
-            BaseDamage;
+            BaseMinDamage,
+            BaseMaxDamage,
+            BaseAttacksPerSecond;
 
         public Entity(string name)
         {
             this.Name = name;
         }
-        public virtual void UpdateBuffs()
+
+        /// <summary>
+        /// Updates the entity's buffs
+        /// </summary>
+        /// <param name="secondsElapsed">the time Elapsed since the last update,in seconds</param>
+        public virtual void UpdateBuffs(double secondsElapsed)
         {
-            
+            //Here we reset the stats
+            CurrentDefense = BaseDefense;
+            CurrentDodge = BaseDodge;
+            MaxLife = BaseLife;
+            MaxMana = BaseMana;
+            CurrentMinDamage = BaseMinDamage;
+            CurrentMaxDamage = BaseMaxDamage; 
+            CurrentMoveSpeed = BaseMoveSpeed;
+            CurrentAttacksPerSecond = BaseAttacksPerSecond;
+            // Here we update the active buffs
+           for(int i = 0;i<Buffs.Count;i++)
+           {
+               Buff b = Buffs[i];
+               CurrentDefense += b.Defense;
+               MaxLife += b.Life;
+               MaxMana += b.Mana;
+               CurrentMinDamage += b.MinDamage;
+               CurrentMaxDamage += b.MaxDamage;
+               CurrentMoveSpeed += BaseMoveSpeed * b.MoveSpeedPerc / 100;
+               CurrentAttacksPerSecond += BaseAttacksPerSecond * b.AttackSpeedPerc / 100;
+           }
+
         }
     }
      
