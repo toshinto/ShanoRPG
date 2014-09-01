b16c9dc883abaa545e72b7375d39ec7894adcd7d
toshinto <lyvcheto@abv.bg>
2014-09-01 18:36:04 +0300

    Interface LifeManaPanel,DmgDefMovePanel

--- ShanoRPGWin/Form1.Designer.cs
+++ ShanoRPGWin/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 301);
+            this.ClientSize = new System.Drawing.Size(624, 358);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
