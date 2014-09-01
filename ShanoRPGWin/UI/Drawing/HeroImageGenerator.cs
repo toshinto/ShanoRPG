c7dacd6250fa2f2b3f6655c734564802f47aa2df
ixtreon <ixtreon@gmail.com>
2014-09-01 17:53:50 +0300

    Added hero picker

---
+++ ShanoRPGWin/UI/Drawing/HeroImageGenerator.cs
@@ -0,0 +1,45 @@
+﻿using ShanoRPGWin.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShanoRPGWin.UI.Drawing
+{
+    class HeroImageGenerator
+    {
+        private static Bitmap VylshebnikBase = Resources.hero;
+
+
+        public static Bitmap GenerateVylshebnik(Color main, Color secondary, Color skin, Color staff)
+        {
+            Bitmap b = VylshebnikBase.Clone(new Rectangle(0, 0, VylshebnikBase.Width, VylshebnikBase.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            b.MakeTransparent(Color.White);
+
+            //var bd = b.LockBits(
+            //    new Rectangle(0, 0, b.Width, b.Height),
+            //    System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            
+            for(int ix = 0; ix < b.Width; ix++)
+                for (int iy = 0; iy < b.Height; iy++)
+                {
+                    var c = b.GetPixel(ix, iy);
+
+                    if (c == Color.FromArgb(0, 0, 255))
+                        b.SetPixel(ix, iy, main);
+                    else if (c == Color.FromArgb(255, 0, 0))
+                        b.SetPixel(ix, iy, secondary);
+                    else if (c == Color.FromArgb(255, 255, 0))
+                        b.SetPixel(ix, iy, skin);
+                    else if (c == Color.FromArgb(0, 255, 0))
+                        b.SetPixel(ix, iy, staff);
+                }
+
+            return b;
+        }
+    }
+}
---
+++ ShanoRPGWin/UI/NewHeroPanel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShanoRPGWin.UI
+{
+    public partial class NewHeroPanel : UserControl
+    {
+        private Color[] skinColors = new[]
+            {
+                Color.FromArgb(255, 255, 128),
+                Color.FromArgb(255, 192, 128),
+                Color.FromArgb(255, 192, 64),
+                Color.FromArgb(192, 100, 50),
+            };
+
+        private Tuple<Color, string>[] staffColors = new[] 
+            {
+                new Tuple<Color, string>(Color.SaddleBrown, "Birchwood"),
+                new Tuple<Color, string>(Color.SandyBrown, "Sandwood"),
+                new Tuple<Color, string>(Color.SaddleBrown, "Saddlewood"),
+                new Tuple<Color, string>(Color.Brown, "Oakwood"),
+                new Tuple<Color, string>(Color.GhostWhite, "Whitewood"),
+            };
+
+        public Color SkinColor { get; private set; }
+        public Color StaffColor { get; private set; }
+        public Color MainColor { get; private set; }
+        public Color SecondaryColor { get; private set; }
+
+        public NewHeroPanel()
+        {
+            InitializeComponent();
+
+            SkinColor = skinColors[0];
+            StaffColor = staffColors[0].Item1;
+            MainColor = Color.DarkGray;
+            SecondaryColor = Color.Red;
+            
+            updateImage();
+
+            foreach (var c in skinColors)
+                skinPicker.Add(c);
+
+            foreach (var staff in staffColors)
+                staffPicker.Add(staff.Item1, staff.Item2);
+
+            mainColorPicker.SelectedColor = MainColor;
+            secondColorPicker.SelectedColor = SecondaryColor;
+        }
+
+        private void skinPicker_ColorSelected(Color c)
+        {
+            SkinColor = c;
+
+            updateImage();
+        }
+
+        private void updateImage()
+        {
+            picBox.Image = ShanoRPGWin.UI.Drawing.HeroImageGenerator.GenerateVylshebnik(MainColor, SecondaryColor, SkinColor, StaffColor);
+        }
+
+        private void staffPicker_ColorSelected(Color c)
+        {
+            StaffColor = c;
+
+            updateImage();
+        }
+
+        private void skinPicker_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainColorPicker_ColorChanged(object sender, EventArgs e)
+        {
+            MainColor = mainColorPicker.SelectedColor;
+
+            updateImage();
+        }
+
+        private void secondColorPicker_ColorChanged(object sender, EventArgs e)
+        {
+            SecondaryColor = secondColorPicker.SelectedColor;
+
+            updateImage();
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            btnNapred.Enabled = txtName.Text.Length > 0;
+        }
+    }
+}
