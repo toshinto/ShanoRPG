2c619fbea223477bf092da7f34f422c7d88c5d0c
toshinto <lyvcheto@abv.bg>
2014-09-08 13:52:57 +0300

    ExpPanel,AttributesPanel

---
+++ ShanoRPGWin/UI/ExpPanel.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ShanoRPGWin.UI
+{
+    partial class ExpPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ExpBar = new System.Windows.Forms.ProgressBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // ExpBar
+            // 
+            this.ExpBar.Location = new System.Drawing.Point(0, 3);
+            this.ExpBar.Name = "ExpBar";
+            this.ExpBar.Size = new System.Drawing.Size(280, 23);
+            this.ExpBar.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.ExpBar, "66/100");
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 100;
+            this.toolTip1.InitialDelay = 100;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // ExpPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ExpBar);
+            this.Name = "ExpPanel";
+            this.Size = new System.Drawing.Size(283, 27);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar ExpBar;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
--- ShanoRPGWin/UI/IngameHeroPanel.Designer.cs
+++ ShanoRPGWin/UI/IngameHeroPanel.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LifeManaPanel = new ShanoRPGWin.UI.LifeManaPanel();
+            this.attributesPanel1 = new ShanoRPGWin.UI.AttributesPanel();
             this.damageDefenseMoveSpeed1 = new ShanoRPGWin.UI.DamageDefenseMoveSpeed();
+            this.LifeManaPanel = new ShanoRPGWin.UI.LifeManaPanel();
+            this.expPanel1 = new ShanoRPGWin.UI.ExpPanel();
             this.SuspendLayout();
+            // 
+            // attributesPanel1
+            // 
+            this.attributesPanel1.Location = new System.Drawing.Point(290, 4);
+            this.attributesPanel1.MaximumSize = new System.Drawing.Size(10000, 122);
+            this.attributesPanel1.MinimumSize = new System.Drawing.Size(89, 122);
+            this.attributesPanel1.Name = "attributesPanel1";
+            this.attributesPanel1.Size = new System.Drawing.Size(89, 122);
+            this.attributesPanel1.TabIndex = 3;
+            // 
+            // damageDefenseMoveSpeed1
+            // 
+            this.damageDefenseMoveSpeed1.Location = new System.Drawing.Point(3, 38);
+            this.damageDefenseMoveSpeed1.MaximumSize = new System.Drawing.Size(10000, 146);
+            this.damageDefenseMoveSpeed1.MinimumSize = new System.Drawing.Size(140, 88);
+            this.damageDefenseMoveSpeed1.Name = "damageDefenseMoveSpeed1";
+            this.damageDefenseMoveSpeed1.Size = new System.Drawing.Size(140, 88);
+            this.damageDefenseMoveSpeed1.TabIndex = 2;
             // 
             // LifeManaPanel
             // 
-            this.LifeManaPanel.Location = new System.Drawing.Point(346, 341);
+            this.LifeManaPanel.Location = new System.Drawing.Point(149, 57);
             this.LifeManaPanel.MaximumSize = new System.Drawing.Size(10000, 60);
             this.LifeManaPanel.MinimumSize = new System.Drawing.Size(104, 60);
             this.LifeManaPanel.Name = "LifeManaPanel";
@@ -42,23 +62,27 @@
             this.LifeManaPanel.TabIndex = 1;
             this.LifeManaPanel.Load += new System.EventHandler(this.LifeManaPanel_Load);
             // 
-            // damageDefenseMoveSpeed1
+            // expPanel1
             // 
-            this.damageDefenseMoveSpeed1.Location = new System.Drawing.Point(499, 326);
-            this.damageDefenseMoveSpeed1.MaximumSize = new System.Drawing.Size(10000, 146);
-            this.damageDefenseMoveSpeed1.MinimumSize = new System.Drawing.Size(140, 88);
-            this.damageDefenseMoveSpeed1.Name = "damageDefenseMoveSpeed1";
-            this.damageDefenseMoveSpeed1.Size = new System.Drawing.Size(140, 88);
-            this.damageDefenseMoveSpeed1.TabIndex = 2;
+            this.expPanel1.Location = new System.Drawing.Point(13, 22);
+            this.expPanel1.MaximumSize = new System.Drawing.Size(1000, 29);
+            this.expPanel1.MinimumSize = new System.Drawing.Size(281, 29);
+            this.expPanel1.Name = "expPanel1";
+            this.expPanel1.Size = new System.Drawing.Size(281, 29);
+            this.expPanel1.TabIndex = 4;
             // 
             // IngameHeroPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.expPanel1);
+            this.Controls.Add(this.attributesPanel1);
             this.Controls.Add(this.damageDefenseMoveSpeed1);
             this.Controls.Add(this.LifeManaPanel);
+            this.MaximumSize = new System.Drawing.Size(10000, 124);
+            this.MinimumSize = new System.Drawing.Size(371, 124);
             this.Name = "IngameHeroPanel";
-            this.Size = new System.Drawing.Size(754, 541);
+            this.Size = new System.Drawing.Size(371, 124);
             this.Load += new System.EventHandler(this.IngameHeroPanel_Load);
             this.ResumeLayout(false);
 
@@ -68,6 +92,8 @@
 
         private LifeManaPanel LifeManaPanel;
         private DamageDefenseMoveSpeed damageDefenseMoveSpeed1;
+        private AttributesPanel attributesPanel1;
+        private ExpPanel expPanel1;
 
 
     }
