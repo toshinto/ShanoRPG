23d7d8f313795a62e6dcc7d8c74174c993076011
ixtreon <ixtreon@gmail.com>
2014-09-08 13:34:58 +0300

    Trqs

--- Engine/ShanoRpg.cs
+++ Engine/ShanoRpg.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using Input;
 
 namespace Engine
 {
@@ -15,6 +16,7 @@ namespace Engine
         /// </summary>
         const int FPS = 60;
 
+        public InputDevice LocalInput;
 
         public readonly Thread MainThread;
 
